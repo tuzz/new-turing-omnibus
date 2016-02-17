26a91a7fb14a224413beb85ffd291222d9ae3ac0
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-17 22:29:31 +0000

    Add the PositionEvaluator

--- 6-game-trees/lib/gameTrees/positionEvaluator.js
+++ 6-game-trees/lib/gameTrees/positionEvaluator.js
@@ -1,7 +1,103 @@
 "use strict";
 
-var PositionEvaluator = function () {
-  // TODO
+var TreeGenerator = require("./treeGenerator");
+
+var PositionEvaluator = function (board, symbol) {
+  var self = this;
+
+  self.evaluate = function () {
+    if (!finished()) {
+      throw new Error("I'm not smart enough to evaluate unfinished games");
+    }
+
+    if (won(symbol)) {
+      return 1;
+    } else if (lost(symbol)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  var won = function (symbol) {
+    return wonAnyRow(symbol) || wonAnyColumn(symbol) || wonAnyDiagonal(symbol);
+  };
+
+  var lost = function (symbol) {
+    return won(otherSymbol(symbol));
+  };
+
+  var finished = function () {
+    for (var y = 0; y < 3; y += 1) {
+      for (var x = 0; x < 3; x += 1) {
+        if (board.get(x, y) === "_") {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  var wonAnyRow = function (symbol) {
+    return wonRow(0, symbol) || wonRow(1, symbol) || wonRow(2, symbol);
+  };
+
+  var wonAnyColumn = function (symbol) {
+    return wonColumn(0, symbol) || wonColumn(1, symbol) || wonColumn(2, symbol);
+  };
+
+  var wonAnyDiagonal = function (symbol) {
+    return wonSouthEast(symbol) || wonNorthEast(symbol);
+  };
+
+  var wonRow = function (y, symbol) {
+    for (var x = 0; x < 3; x += 1) {
+      if (board.get(x, y) !== symbol) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  var wonColumn = function (x, symbol) {
+    for (var y = 0; y < 3; y += 1) {
+      if (board.get(x, y) !== symbol) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  var wonSouthEast = function (symbol) {
+    for (var i = 0; i < 3; i += 1) {
+      if (board.get(i, i) !== symbol) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  var wonNorthEast = function (symbol) {
+    for (var i = 0; i < 3; i += 1) {
+      if (board.get(i, 2 - i) !== symbol) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  var otherSymbol = function (symbol) {
+    return symbol === "X" ? "O" : "X";
+  };
+};
+
+PositionEvaluator.evaluate = function (board, symbol) {
+  return new PositionEvaluator(board, symbol).evaluate();
 };
 
 module.exports = PositionEvaluator;
--- 6-game-trees/spec/gameTrees/positionEvaluatorSpec.js
+++ 6-game-trees/spec/gameTrees/positionEvaluatorSpec.js
@@ -1,9 +1,53 @@
 "use strict";
 
 var DescribedClass = require("../../lib/gameTrees/positionEvaluator");
+var Board = require("../../lib/gameTrees/board");
 
 describe("PositionEvaluator", function () {
-  it("has some tests", function () {
-    expect(true).toEqual(false, "Implement me");
+  var winningBoard, drawnBoard;
+
+  beforeEach(function () {
+    winningBoard = new Board([
+      ["X", "X", "X"],
+      ["O", "X", "O"],
+      ["O", "X", "O"]
+    ]);
+
+    drawnBoard = new Board([
+      ["X", "O", "X"],
+      ["O", "O", "X"],
+      ["X", "X", "O"]
+    ]);
+  });
+
+  it("returns 1 if the player has won", function () {
+    expect(DescribedClass.evaluate(winningBoard, "X")).toEqual(1);
+  });
+
+  it("returns -1 if the player has lost", function () {
+    expect(DescribedClass.evaluate(winningBoard, "O")).toEqual(-1);
+  });
+
+  it("returns 0 if the player drew", function () {
+    expect(DescribedClass.evaluate(drawnBoard, "X")).toEqual(0);
+    expect(DescribedClass.evaluate(drawnBoard, "O")).toEqual(0);
+  });
+
+  describe("when the game is unfinished", function () {
+    var unfinishedBoard;
+
+    beforeEach(function () {
+      unfinishedBoard = new Board([
+        ["X", "X", "X"],
+        ["O", "_", "O"],
+        ["O", "X", "O"]
+      ]);
+    });
+
+    it("throws an error", function () {
+      expect(function () {
+        DescribedClass.evaluate(unfinishedBoard, "X");
+      }).toThrow();
+    });
   });
 });
