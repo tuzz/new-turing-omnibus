5ab88bdd38d0aaf526b9031aa4b9531088d6bb57
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-18 00:01:34 +0000

    Add the MinimaxProcedure

--- 6-game-trees/lib/gameTrees/minimaxProcedure.js
+++ 6-game-trees/lib/gameTrees/minimaxProcedure.js
@@ -1,7 +1,43 @@
 "use strict";
 
-var MinimaxProcedure = function () {
-  // TODO
+var MinimaxProcedure = function (tree) {
+  var self = this;
+
+  self.run = function () {
+    var leaves = tree.leaves();
+    climbTree(leaves, false);
+  };
+
+  var climbTree = function (nodes, maximise) {
+    var uniqueParents = [];
+    var trackedParent = {};
+
+    for (var i = 0; i < nodes.length; i += 1) {
+      var node = nodes[i];
+      var parent = node.parent;
+
+      if (!parent) {
+        return;
+      } else if (!parent.value) {
+        parent.value = node.value;
+      } else if (maximise && node.value > parent.value) {
+        parent.value = node.value;
+      } else if (!maximise && node.value < parent.value) {
+        parent.value = node.value;
+      }
+
+      if (!trackedParent[parent]) {
+        uniqueParents.push(parent);
+        trackedParent[parent] = true;
+      }
+    }
+
+    climbTree(uniqueParents, !maximise);
+  };
+};
+
+MinimaxProcedure.run = function (tree) {
+  new MinimaxProcedure(tree).run();
 };
 
 module.exports = MinimaxProcedure;
--- 6-game-trees/spec/gameTrees/minimaxProcedureSpec.js
+++ 6-game-trees/spec/gameTrees/minimaxProcedureSpec.js
@@ -1,9 +1,44 @@
 "use strict";
 
 var DescribedClass = require("../../lib/gameTrees/minimaxProcedure");
+var Tree = require("../../lib/gameTrees/tree");
+var Node = require("../../lib/gameTrees/node");
 
 describe("MinimaxProcedure", function () {
-  it("has some tests", function () {
-    expect(true).toEqual(false, "Implement me");
+  var tree, root, child1, child2,
+    grandchild1, grandchild2, grandchild3, grandchild4;
+
+  beforeEach(function () {
+    root = new Node("root");
+    child1 = new Node("child1");
+    child2 = new Node("child2");
+    grandchild1 = new Node("grandchild1");
+    grandchild2 = new Node("grandchild2");
+    grandchild3 = new Node("grandchild3");
+    grandchild4 = new Node("grandchild4");
+
+    root.addChild(child1);
+    root.addChild(child2);
+
+    child1.addChild(grandchild1);
+    child1.addChild(grandchild2);
+
+    child2.addChild(grandchild3);
+    child2.addChild(grandchild4);
+
+    tree = new Tree(root);
+
+    grandchild1.value = 2;
+    grandchild2.value = 7;
+    grandchild3.value = 1;
+    grandchild4.value = 8;
+  });
+
+  it("populates the values tree nodes according to minimax", function () {
+    DescribedClass.run(tree);
+
+    expect(child1.value).toEqual(2);
+    expect(child2.value).toEqual(1);
+    expect(root.value).toEqual(2);
   });
 });
